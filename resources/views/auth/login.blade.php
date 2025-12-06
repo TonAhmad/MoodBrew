fdd757ada6798fbe605a828ad4dde0f55a032986
Aidan-Joe <aidanjoe88@gmail.com>
2025-12-06 13:00:32 +0700

    fix login staff issue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,7 @@
                     </div>
                 @endif
 
-                <form method="POST" action="{{ route('login') }}" class="space-y-6">
+                <form method="POST" action="{{ route('staff.login') }}" class="space-y-6">
                     @csrf
 
                     {{-- Email --}}
